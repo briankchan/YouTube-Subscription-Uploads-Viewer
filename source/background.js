fed56f5213510b459e9e85fc47031c58db8dca9e
iluvredwall <bobomb.asplode@gmail.com>
2014-12-14 23:17:41 -0800

    improve handling of viewer opening/focusing

--- source/background.js
+++ source/background.js
@@ -1,12 +1,19 @@
-
-var tabID;
+/**
+ * Background script for the extension.
+ * Handles opening/closing of the viewer tab and schedules fetching of upload videos.
+ */
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-	if(tabID == null) {
-		chrome.tabs.create({url:"viewer.html"}, function(tab) {
-			tabID = tab.id;
-		});
+	var openTabs = chrome.extension.getViews({ type: "tab" });
+	
+	if(openTabs.length < 1) {
+		chrome.tabs.create({ url: "viewer.html" });
 	} else {
-		chrome.tabs.update(tabID, {selected : true});
+		openTabs[0].chrome.tabs.getCurrent(function(tab){
+			chrome.tabs.update(tab.id, { highlighted: true });
+			chrome.tabs.get(tab.id, function(tab) {
+				chrome.windows.update(tab.windowId, { focused: true });
+			});
+		});
 	}
 });
