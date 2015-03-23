c9cbbfeb04fe3fcfa476515397f75ee7ad6a8b4e
iluvredwall <bobomb.asplode@gmail.com>
2015-03-22 18:07:58 -0700

    refactor youtube-resource.js to reduce redundancy

--- source/script/youtube-resource.js
+++ source/script/youtube-resource.js
@@ -2,31 +2,26 @@
 //var API_KEY = "AIzaSyClOj2RmQTkYbfqL4o8mBhzx8Jwo-mNhpo"; //not actually needed with auth tokens
 var BASE_URL = "https://www.googleapis.com/youtube/v3/";
 
-var authToken;
-
 module.exports = YoutubeResource;
+module.exports.setAuthToken = function(token) {
+	YoutubeResource.authToken = token;
+};
+
 function YoutubeResource(resource) {
 	this.resource = resource;
 }
 
-module.exports.setAuthToken = function(token) {
-	authToken = token; //TODO put in prototype?
-};
 YoutubeResource.prototype.get    = function(options) { return this.ajax("GET", options); };
 YoutubeResource.prototype.post   = function(options) { return this.ajax("POST", options); };
 YoutubeResource.prototype.put    = function(options) { return this.ajax("PUT", options); };
 YoutubeResource.prototype.delete = function(options) { return this.ajax("DELETE", options); };
 YoutubeResource.prototype.ajax = function(method, options) {
-	return sendRequest(this.resource, method, options);
-};
-
-function sendRequest(resource, method, options) {
 	var deferred = $.Deferred();
 	
 	$.ajax({
-		url: BASE_URL + resource,
+		url: BASE_URL + this.resource,
 		type: method,
-		headers: { authorization: "Bearer " + authToken },
+		headers: { authorization: "Bearer " + YoutubeResource.authToken },
 		data: options
 	}).fail(function(jqXHR, textStatus, errorThrown) {
 		console.error(textStatus + " " + errorThrown + " trying to access YouTube Data API");
@@ -36,4 +31,4 @@ function sendRequest(resource, method, options) {
 	});
 	
 	return deferred.promise();
-}
+};
